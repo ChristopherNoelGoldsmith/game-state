fe91445ba18c752dad2c484c7c1895064474cb78
Christopher Goldsmith <christophergoldsmith101@yahoo.com>
2022-06-09 04:42:24 -0700

    heart image added to health

--- src/components/counters/Block.jsx
+++ src/components/counters/Block.jsx
@@ -1,7 +1,9 @@
 import Value from "./incrimentors/Value";
 
 const Block = props => {
-    return <Value/>
+    return (<div id="block">
+        <Value/>
+        </div>)
 }
 
 export default Block;
